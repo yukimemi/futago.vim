4829129a84b2f9389c0510b6f3aa1ceffebee453
yukimemi <yukimemi@gmail.com>
2024-02-04 21:08:03 +0900

    fix: use `diff --cached` instead of `diff --staged`

--- denops/futago/consts.ts
+++ denops/futago/consts.ts
@@ -1,7 +1,7 @@
 // =============================================================================
 // File        : consts.ts
-// Last Change : 2024/01/28 11:33:58.
-// Last Change : 2024/01/28 11:33:58.
+// Author      : yukimemi
+// Last Change : 2024/02/04 20:28:38.
 // =============================================================================
 
 import { Opener } from "./schema/opener.ts";
@@ -13,3 +13,20 @@ export const DEFAULT_AI_PROMPT = "Gemini";
 export const DEFAULT_HUMAN_PROMPT = "You";
 
 export const SEPARATOR = "-------------";
+
+export const GIT_COMMIT_PROMPT = `
+Please make git commit messages for the following diff output.
+
+Each commit message must be one line starting with one of the following words.
+
+* feat: (new feature for the user, not a new feature for build script)
+* fix: (bug fix for the user, not a fix to a build script)
+* docs: (changes to the documentation)
+* style: (formatting, missing semi colons, etc; no production code change)
+* refactor: (refactoring production code, eg. renaming a variable)
+* test: (adding missing tests, refactoring tests; no production code change)
+* chore: (updating grunt tasks etc; no production code change)
+
+### diff
+
+`;
--- denops/futago/dispatcher/git_commit.ts
+++ denops/futago/dispatcher/git_commit.ts
@@ -1,14 +1,14 @@
 // =============================================================================
 // File        : git_commit.ts
 // Author      : yukimemi
-// Last Change : 2024/02/04 20:15:07.
+// Last Change : 2024/02/04 20:29:33.
 // =============================================================================
 
 import * as fn from "https://deno.land/x/denops_std@v6.0.1/function/mod.ts";
 import { Futago } from "../futago.ts";
 import { type Denops } from "https://deno.land/x/denops_core@v6.0.5/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
-import { DEFAULT_MODEL } from "../consts.ts";
+import { DEFAULT_MODEL, GIT_COMMIT_PROMPT } from "../consts.ts";
 import { SafetySettingsSchema } from "../schema/safety_settings.ts";
 import { GenerationConfigSchema } from "../schema/generation_config.ts";
 
@@ -35,7 +35,7 @@ async function git(base: string, args: string[]): Promise<string> {
 }
 
 async function getDiffStaged(base: string): Promise<string> {
-  return await git(base, ["diff", "--staged"]);
+  return await git(base, ["diff", "--cached"]);
 }
 
 export async function gitCommit(
@@ -64,23 +64,7 @@ export async function gitCommit(
   );
 
   const diff = await getDiffStaged(await getGitRoot());
-  const p = (prompt ??
-    `
-Please make git commit messages for the following diff output.
-
-Each commit message must be one line starting with one of the following words.
-
-* feat: (new feature for the user, not a new feature for build script)
-* fix: (bug fix for the user, not a fix to a build script)
-* docs: (changes to the documentation)
-* style: (formatting, missing semi colons, etc; no production code change)
-* refactor: (refactoring production code, eg. renaming a variable)
-* test: (adding missing tests, refactoring tests; no production code change)
-* chore: (updating grunt tasks etc; no production code change)
-
-### diff
-  `) +
-    "\n\n" + diff;
+  const p = (prompt ?? GIT_COMMIT_PROMPT) + "\n\n" + diff;
   const commitMessage = await futago.generateContent(p);
   console.log(commitMessage);
 
